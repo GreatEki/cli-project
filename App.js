75917efbbe8a8843fce307e7da01b35c30595cff
GreatEki <ekeneidiagbor@gmail.com>
2022-03-10 16:05:04 +0100

    textinputs in react-native

--- App.js
+++ App.js
@@ -8,8 +8,7 @@
 
 import React, {useState} from 'react';
 // import type {Node} from 'react';
-import {StyleSheet, View, Text, FlatList} from 'react-native';
-import SectionList from 'react-native/Libraries/Lists/SectionList';
+import {StyleSheet, View, Text, TextInput} from 'react-native';
 
 // import {
 //   Colors,
@@ -22,67 +21,28 @@ const App = () => {
   //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   // };
 
-  const [items, setItems] = useState([
-    {name: 'Item 1'},
-    {name: 'Item 2'},
-    {name: 'Item 3'},
-    {name: 'Item 4'},
-    {name: 'Item 5'},
-    {name: 'Item 6'},
-    {name: 'Item 7'},
-    {name: 'Item 8'},
-  ]);
+  const [name, setName] = useState('');
 
-  const DATA = [
-    {
-      title: 'title1',
-      data: ['nameTitle1-1', 'nameTitle1-2', 'nameTitle1-3'],
-    },
-    {
-      title: 'title2',
-      data: ['nameTitle2', 'nameTitle2-1'],
-    },
-    {
-      title: 'title3',
-      data: ['nameTitle3', 'nameTitle3-1', 'nameTitle3-2'],
-    },
-    {
-      title: 'title4',
-      data: ['nameTitle4', 'nameTitle4-1', 'nameTitle4-2'],
-    },
-  ];
+  function onChangeText(val) {
+    setName(val);
+  }
 
   return (
     <View style={styles.body}>
-      {/* <ScrollView> */}
-      <SectionList
-        keyExtractor={(item, index) => index.toString()}
-        sections={DATA}
-        renderItem={({item}) => <Text style={styles.text}> {item} </Text>}
-        renderSectionHeader={({section}) => (
-          <View style={styles.item}>
-            <Text style={styles.text}> {section.title} </Text>
-          </View>
-        )}
-      />
-      <FlatList
-        // horizontal
-        keyExtractor={(item, index) => index.toString()}
-        data={items}
-        renderItem={({item}) => (
-          <View style={styles.item}>
-            <Text style={styles.text}>{item.name}</Text>
-          </View>
-        )}
-      />
-      {/* <ScrollView style={styles.scroll}>
-        {items.map((element, index) => (
-          <View key={index} style={styles.item}>
-            <Text style={styles.text}>{element.item}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
-      {/* </ScrollView> */}
+      <View style={styles.item}>
+        <Text style={styles.text}>Please write your name</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          secureTextEntry
+          // editable={false}
+          // multiline
+        />
+      </View>
+
+      <View>
+        <Text> Your name is: {name}</Text>
+      </View>
     </View>
   );
 };
@@ -91,13 +51,14 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
     borderColor: 'pink',
     borderRadius: 10,
+    justifyContent: 'center',
   },
 
   text: {
-    fontSize: 50,
+    fontSize: 20,
     fontStyle: 'italic',
     fontWeight: 'bold',
     margin: 10,
@@ -106,11 +67,15 @@ const styles = StyleSheet.create({
 
   item: {
     // backgroundColor: '#4ae1fa',
-    backgroundColor: 'green',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     margin: 10,
+  },
+
+  input: {
+    borderWidth: 1,
+    width: '95%',
   },
 });
 
