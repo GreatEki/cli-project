c3f6ee1ec0903b91b635a4a441ff5464a1ce0097
GreatEki <ekeneidiagbor@gmail.com>
2022-03-30 15:23:04 +0100

    working with tabs and tabicons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {LogBox} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
+// import {createStackNavigator } from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ScreenA from './screens/ScreenA/ScreenA';
 import ScreenB from './screens/ScreenB/ScreenB';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
+import Path from './constants/urls';
 const App = () => {
-  const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
 
   LogBox.ignoreLogs([
     "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
@@ -15,13 +17,25 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          header: () => null,
-        }}>
-        <Stack.Screen name="ScreenA" component={ScreenA} />
-        <Stack.Screen name="ScreenB" component={ScreenB} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, size, color}) => {
+            let iconName;
+            if (route.name === Path.ScreenA) {
+              iconName = 'autoprefixer';
+              size = focused ? 25 : 20;
+              color = focused ? 'blue' : '#555';
+            } else if (route.name === Path.ScreenB) {
+              iconName = 'btc';
+              size = focused ? 25 : 20;
+              color = focused ? 'blue' : '#555';
+            }
+            return <FontAwesome5 name={iconName} size={size} color={color} />;
+          },
+        })}>
+        <Tab.Screen name="ScreenA" component={ScreenA} />
+        <Tab.Screen name="ScreenB" component={ScreenB} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
